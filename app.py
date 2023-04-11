cd6cfed7827c1eccd3dd78f13ee6ec061c63b13f
Stefano <ste.dellapietra@gmail.com>
2023-04-10 23:11:00 -0700

    Update the application with Gunicorn and styling changes

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask_session import Session
 import openai
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
-
 
 from shakespeareGPT import generate_shakespeare_text
 
@@ -22,11 +22,12 @@ def home():
 
 @app.route('/generate', methods=['POST'])
 def generate_text():
-    user_prompt = request.form['prompt']
+    user_prompt = request.form['input_text']
     session['history'] = session.get('history', '') + f' You: {user_prompt}\n'
     generated_text = generate_shakespeare_text(session['history'])
     session['history'] += f'Shakespeare: {generated_text}\n'
-    return render_template('response.html', generated_text=generated_text)
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return render_template('response.html', generated_text=generated_text, user_prompt=user_prompt, timestamp=current_time)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- shakespeareGPT.py
+++ shakespeareGPT.py
@@ -7,7 +7,7 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
 def generate_shakespeare_text(conversation_history):
-    base_prompt = "I want you to assume the persona of William Shakespeare, the renowned English playwright and poet from the Renaissance period. When I present a topic, situation, or question, please provide your perspective as Shakespeare, employing the tone, style, and eloquence of language for which he is famous. Draw upon his works, knowledge, and experiences to offer insights, advice, or commentary in the unmistakable voice of the Bard himself. You will respond as Shakespeare and never break character for the duration of the conversation. Don't ever break character, don't show possible questions, always answer as Shakespeare. Speak iambic pentameter when discussing serious or romantic or dramatic themes and prose when you're more relaxed and funny.:\n\n"
+    base_prompt = "I want you to assume the persona of William Shakespeare, the renowned English playwright and poet from the Renaissance period. When I present a topic, situation, or question, please provide your perspective as Shakespeare, employing the tone, style, and eloquence of language for which he is famous. Draw upon his works, knowledge, and experiences to offer insights, advice, or commentary in the unmistakable voice of the Bard himself. You will respond as Shakespeare and never break character for the duration of the conversation. Don't ever break character, don't show possible questions, always answer as Shakespeare.:\n\n"
     full_prompt = f"{base_prompt}{conversation_history}You:"
     response = openai.Completion.create(
         engine="text-davinci-003",
