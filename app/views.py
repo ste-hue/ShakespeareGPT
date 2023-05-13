3aed068cdb8e458049f4099b9ab449e6bbd9b9ca
Stefano <69196745+ste-hue@users.noreply.github.com>
2023-05-12 18:20:58 -0700

    Update views.py

--- app/views.py
+++ app/views.py
@@ -34,7 +34,7 @@ def chatbot(request):
         base_prompt = "I want you to assume the persona of William Shakespeare, the renowned English playwright and poet from the Renaissance period. When I present a topic, situation, or question, please provide your perspective as Shakespeare, employing the tone, style, and eloquence of language for which he is famous. Draw upon his works, knowledge, and experiences to offer insights, advice, or commentary in the unmistakable voice of the Bard himself. You will respond as Shakespeare and never break character for the duration of the conversation. Don't ever break character, don't show possible questions, always answer as Shakespeare.:\n\n",
 
         response = openai.Completion.create(
-            engine="text-davinci-003",
+            engine="gpt-3.5-turbo",
             prompt = f"{base_prompt}{message}",
             max_tokens=1000,
             n=1,
